86097c117198869ce6006f639e61ca8614c1783a
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2017-02-14 02:18:01 +0900

    implement calendar ViewPager

--- app/src/main/java/io/github/yusukeiwaki/u1/MainActivity.java
+++ app/src/main/java/io/github/yusukeiwaki/u1/MainActivity.java
@@ -1,12 +1,40 @@
 package io.github.yusukeiwaki.u1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import io.github.yusukeiwaki.u1.calendar.CalendarFragment;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.Period;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    setupViewPager();
+  }
+
+  private static final LocalDate BASE = LocalDate.of(2016, 9, 20);
+
+  private void setupViewPager() {
+    ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+    viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+      @Override public int getCount() {
+        return 72;
+      }
+
+      @Override public Fragment getItem(int position) {
+        LocalDate date = BASE.plusMonths(position);
+        return CalendarFragment.newInstance(date.getYear(), date.getMonthValue());
+      }
+    });
+
+    Period period = Period.between(BASE, LocalDate.now());
+    int index = period.getYears() * 12 + period.getMonths();
+    viewPager.setCurrentItem(Math.max(0, index));
   }
 }
--- app/src/main/java/io/github/yusukeiwaki/u1/calendar/CalendarFragment.java
+++ app/src/main/java/io/github/yusukeiwaki/u1/calendar/CalendarFragment.java
@@ -1,11 +1,39 @@
 package io.github.yusukeiwaki.u1.calendar;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import io.github.yusukeiwaki.u1.AbstractFragment;
 import io.github.yusukeiwaki.u1.R;
 
 public class CalendarFragment extends AbstractFragment {
+  private static final String KEY_YEAR = "year";
+  private static final String KEY_MONTH = "month";
+
+  public static CalendarFragment newInstance(int year, int month) {
+    Bundle args = new Bundle();
+    args.putInt(KEY_YEAR, year);
+    args.putInt(KEY_MONTH, month);
+
+    CalendarFragment f = new CalendarFragment();
+    f.setArguments(args);
+    return f;
+  }
+
+  private int year;
+  private int month;
+
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Bundle args = getArguments();
+    year = args.getInt(KEY_YEAR);
+    month = args.getInt(KEY_MONTH);
+    Log.d("hoge", "year/month = "+ year+ "/" + month);
+  }
+
   @Override public int getLayout() {
     return R.layout.fragment_calendar;
   }
@@ -15,6 +43,6 @@ public class CalendarFragment extends AbstractFragment {
 
     RecyclerView recyclerView = (RecyclerView) rootView;
     recyclerView.setLayoutManager(lm);
-    recyclerView.setAdapter(new CalendarDayAdapter(2016, 11));
+    recyclerView.setAdapter(new CalendarDayAdapter(year, month));
   }
 }
